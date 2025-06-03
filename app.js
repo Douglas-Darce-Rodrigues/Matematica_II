bab119f61a7a47ff3777fa88793dd838230dd78f
Douglas Darce <douglas.arce.rodrigues@escola.pr.gov.br>
2025-06-02 23:35:52 -0300

    a

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
-    cartao.classname = 'cartao'
+    cartao.className = 'cartao'
 
     cartao.innerHTML = `
-<div class="cartao__conteudo">
-                    <h3>${categoria}</h3>
-                    <div class="cartao__conteudo__pergunta"><p>${pergunta}</p></div>
-                    <div class="cartao__conteudo__resposta"><p>${resposta}</p></div>
-                </div>
-`
-container.appendChild(cartao)
-} 
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
+    container.appendChild(cartao)
+
+}
